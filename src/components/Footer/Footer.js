b062246333062258cb51458e485a918a4d581cc8
JRivenburgh <jurassicjosh429@gmail.com>
2019-01-15 23:40:53 -0800

    commit4

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,9 @@ const Footer = () => (
     <footer className="footer">
         <div className="bottom">
             <ul id='footer-list'>
-                <li><span><img className='logo' id="reactLogo" src="assets/images/react-logo.png" alt= "react logo"/></span> Clicky Game!</li>
+                <li><span><img className='logo' id="reactLogo" src="https://raw.githubusercontent.com/Jrivenburgh/clicky/master/public/assets/images/react-logo.png" alt= "react logo"/></span> Clicky Game!</li>
                 <li>© 2019 Joshua Rivenburgh</li>
+                <li><a href="https://github.com/jrivenburgh/clicky" target="blank"><span><img className='logo' src="https://raw.githubusercontent.com/Jrivenburgh/clicky/master/public/assets/images/github.png" alt="github logo"/></span> Github</a></li>
             </ul>
         </div>
     </footer>
--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -4,7 +4,7 @@ import "./Instructions.css";
 
 const Instructions = (props) => (
     <div className="jumbo">
-        <Jumbotron style={{'background-image': `url("public/assets/images/Stadium.png")`}} >
+        <Jumbotron style={{'background-image': `url("https://raw.githubusercontent.com/Jrivenburgh/clicky/master/public/assets/images/Stadium.png")`}} >
             <h3 className=" animated fadeInDown instructions">{props.message}</h3>
         </Jumbotron>
     </div>
