195eaae42c41ff7d373d9b86ffdbf033113cdc32
JRivenburgh <jurassicjosh429@gmail.com>
2019-01-15 23:10:26 -0800

    commit2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => (
     <footer className="footer">
         <div className="bottom">
             <ul id='footer-list'>
-                <li><span><img className='logo' id="reactLogo" src="assets/images/react-logo.png"/></span> Clicky Game!</li>
+                <li><span><img className='logo' id="reactLogo" src="assets/images/react-logo.png" alt= "react logo"/></span> Clicky Game!</li>
                 <li>© 2019 Joshua Rivenburgh</li>
-                <li><a href="https://github.com/jrivenburgh/clicky" target="blank"><span><img className='logo' src="public/assets/images/github.png"/></span> Github</a></li>
+                <li><a href="https://github.com/jrivenburgh/clicky" target="blank"><span><img className='logo' src="" alt= "github logo"/></span> Github</a></li>
             </ul>
         </div>
     </footer>
